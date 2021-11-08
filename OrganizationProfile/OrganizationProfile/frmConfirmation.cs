b4b08a4d693c2a3882cf6247d5d00a12b65e8b72
CienR <91470864+CienR@users.noreply.github.com>
2021-11-08 16:04:41 +0800

    Add files via upload

---
+++ OrganizationProfile/OrganizationProfile/frmConfirmation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationProfile
+{
+    public partial class frmConfirmation : Form
+    {
+        public frmConfirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+                    }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
